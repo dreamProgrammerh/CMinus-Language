4527ad4b676f038ecb4343d8fdae424fec2e8734
DreamProgrammer <99367269+dreamProgrammerh@users.noreply.github.com>
2025-12-11 17:53:10 +0330

    Create include/c-minus.h

---
+++ include/c-minus.h
@@ -0,0 +1,22 @@
+/*
+ * CMinus Language Standard
+ * Version: 1.0.0 (Standard 2026)
+ * 
+ * A minimal, clean layer over C that reduces boilerplate
+ * while generating optimized C code.
+ */
+#pragma once
+
+#define __cminus__
+
+/* Language Standard Version */
+#define __cminus_std_year__ 2026
+#define __cminus_std_version__ 1
+#define __cminus_std_string__ "cm126"
+#define __cminus_std__ 0xc260100 /* Year 2026, Version 1.00 */
+
+/* Implementation Version */
+#define __cminus_v_major__ 1
+#define __cminus_v_minor__ 0
+#define __cminus_v_patch__ 0
+#define __cminus_v_str__ "1.0.0"
